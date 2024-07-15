8f888d0583a2cda4e16f0e667195f1e42d8fd46a
AliAsgar Padaria <aliasgarpadaria002@gmail.com>
2024-07-15 18:59:13 +0530

    Portfolio v0.1

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      'animation': {
+            'text':'5s ease infinite',
+        },
+        'keyframes': {
+            'text': {
+                '0%, 100%': {
+                   'background-size':'200% 200%',
+                    'background-position': 'left center'
+                },
+                '50%': {
+                   'background-size':'200% 200%',
+                    'background-position': 'right center'
+                }
+            },
+        }
+    },
+  },
+  plugins: [  
+    require('flowbite/plugin')
+  ],
+}
+
